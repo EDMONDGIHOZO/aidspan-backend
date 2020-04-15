89829d83dddb5afbc27b4ed9e775b0f55dffd7a3
tytyne <dusaflora2@gmail.com>
2020-04-15 17:59:25 +0200

    add  documentation

--- backend/start/app.js
+++ backend/start/app.js
@@ -16,6 +16,8 @@ const providers = [
   "@adonisjs/bodyparser/providers/BodyParserProvider",
   "@adonisjs/cors/providers/CorsProvider",
   "@adonisjs/lucid/providers/LucidProvider",
+  "adonis-swagger/providers/SwaggerProvider",
+  "adonis-swagger-jsdoc/providers/SwaggerjsdocProvider",
 ];
 
 /*
